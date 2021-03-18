34ed0d10bd32943bef83e51961ea78cbb6ca52f4
Erik Heumrønningen <erik.heumronningen@creuna.no>
2021-03-19 00:40:23 +0100

    Adds video type to sanity schema.

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -2,9 +2,10 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import Page from "./page";
-import ExtendedImage from "./extendedImage";
+import ExtendedImage from "./extended-image";
+import Video from "./video";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([Page, ExtendedImage]),
+  types: schemaTypes.concat([Page, ExtendedImage, Video]),
 });
