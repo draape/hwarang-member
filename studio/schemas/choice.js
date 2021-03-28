69a9a6f87295d062ae8bf2b7580a66914ec63d25
Erik Heumrønningen <erik@draape.net>
2021-03-28 22:48:05 +0200

    Adds quiz schema (#13)

---
+++ studio/schemas/choice.js
@@ -0,0 +1,39 @@
+export default {
+  name: "choice",
+  title: "Choice",
+  type: "object",
+  fields: [
+    {
+      name: "title",
+      title: "Tittel",
+      type: "string",
+      validation: (Rule) =>
+        Rule.custom((title) => {
+          if (!title) {
+            return "Tittelen må ha en verdi";
+          }
+          if (title.length > 300) {
+            return `Et alternativ kan ikke har mer enn 300 tegn. Denne har ${title.length}`;
+          }
+          return true;
+        }),
+    },
+    {
+      name: "isCorrect",
+      title: "Riktig svar",
+      type: "boolean",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      isCorrect: "isCorrect",
+    },
+    prepare({ title, isCorrect }) {
+      return {
+        title: `${isCorrect ? "✅" : "❌"} ${title}`,
+      };
+    },
+  },
+};
---
+++ studio/schemas/player.js
@@ -0,0 +1,15 @@
+import { MdFace } from "react-icons/md";
+
+export default {
+  name: "player",
+  title: "Spiller",
+  type: "document",
+  icon: MdFace,
+  fields: [
+    {
+      name: "name",
+      title: "Navn",
+      type: "string",
+    },
+  ],
+};
