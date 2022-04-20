90975cf4737b8ccb891cda96ac3283a994aa197f
Erik Heumrønningen <erik.heumronningen@knowit.no>
2022-04-20 23:07:43 +0200

    Adds a grid component used for laying out cards

---
+++ web/src/components/grid/grid.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+
+export const Grid: FC = ({ children }) => (
+  <div className="grid">{children}</div>
+);
--- web/src/pages/video-gallery.tsx
+++ web/src/pages/video-gallery.tsx
@@ -8,6 +8,7 @@ import { getDistinctCategories } from "../utils/get-distinct-categories";
 import Layout from "../components/layout/layout";
 import { FacetList } from "../components/facet-list/facet-list";
 import { Card } from "../components/card/card";
+import { Grid } from "../components/grid/grid";
 
 interface VideoGalleryProps {
   data: {
@@ -27,7 +28,7 @@ const VideoGallery: React.FC<VideoGalleryProps> = ({
     allSanityVideo: { videos },
   },
 }) => {
-  const [category, setCategory] = useState<string>();
+  const [category, setCategory] = useState<string>(null);
   const categories = getDistinctCategories(videos);
 
   const onSelect = (value: string) => setCategory(value);
@@ -36,21 +37,23 @@ const VideoGallery: React.FC<VideoGalleryProps> = ({
     <Layout>
       <h1>Videoer</h1>
       <FacetList categories={categories} onSelect={onSelect} />
-      {videos.map((video) => {
-        const image = getImage(video.thumbnail);
-        return (
-          (category === null || category === video.category.slug.current) && (
-            <Card
-              key={video.slug.current}
-              title={video.title}
-              subtitle={video.category.title}
-              link={`/video/${video.slug.current}`}
-              image={image}
-              imageAlt={video.title}
-            />
-          )
-        );
-      })}
+      <Grid>
+        {videos.map((video) => {
+          const image = getImage(video.thumbnail);
+          return (
+            (category === null || category === video.category.slug.current) && (
+              <Card
+                key={video.slug.current}
+                title={video.title}
+                subtitle={video.category.title}
+                link={`/video/${video.slug.current}`}
+                image={image}
+                imageAlt={video.title}
+              />
+            )
+          );
+        })}
+      </Grid>
     </Layout>
   );
 };
